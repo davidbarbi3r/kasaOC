f3b2ab3fa41339899f254f0e1c599cbdc00815ca
davidbarbi3r <davidbarbi3r@gmail.com>
2023-06-16 16:59:12 +0200

    :sparkles: handling unexpected id

--- src/Accomodation/Accomodation.jsx
+++ src/Accomodation/Accomodation.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import data from "../assets/data.json"
 import { useLocation } from 'react-router-dom'
+import NoMatch from '../NoMatch/NoMatch'
 
 export default function Accomodation() {
   const {pathname} = useLocation()
   const currentAccomodation = data.filter((accomodation) => "/" + accomodation.id === pathname )[0]
+
+  if (!currentAccomodation) {
+    return <NoMatch/>
+  }
 
   return (
     <div>{currentAccomodation.title}</div>
