ee717a376a6d53449d0693163d2426e6797f646f
davidbarbi3r <davidbarbi3r@gmail.com>
2023-06-16 14:51:30 +0200

    :lipstick: add underline in navbar / page

--- src/Layout/Components/Navbar.jsx
+++ src/Layout/Components/Navbar.jsx
@@ -1,32 +1,31 @@
 import './Navbar.css'
 import React from "react";
-import { 
-    Link, 
-    // useParams 
-} from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Navbar () {
-    // const currentPath = useParams()
+    const { pathname } = useLocation();
 
     return (
-        <div className="navbar-container">
-            <Link to="/">
-                <div className="navbar-logo">
-                        <img src="/src/assets/logo-K.svg" />
-                        <img src="/src/assets/logo-home.svg" />
-                        <img src="/src/assets/logo-S.svg" />
-                        <img src="/src/assets/logo-A.svg" />
-                </div>
-            </Link>
-            <ul>
-                <Link to="/">
-                    <li>Accueil</li>
-                </Link>
-                <Link to="a-propos">
-                    <li>A propos</li>
-                </Link>
-            </ul>
-
-        </div>
-    )
+      <div className="navbar-container">
+        <Link to="/">
+          <div className="navbar-logo">
+            <img src="/src/assets/logo-K.svg" />
+            <img src="/src/assets/logo-home.svg" />
+            <img src="/src/assets/logo-S.svg" />
+            <img src="/src/assets/logo-A.svg" />
+          </div>
+        </Link>
+        <ul>
+          <Link to="/" className={pathname === "/" ? "underline" : ""}>
+            <li>Accueil</li>
+          </Link>
+          <Link
+            to="a-propos"
+            className={pathname === "/a-propos" ? "underline" : ""}
+          >
+            <li>A propos</li>
+          </Link>
+        </ul>
+      </div>
+    );
 }
