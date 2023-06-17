6770ee51c19b35f06750f9625b869fd417b02c69
davidbarbi3r <davidbarbi3r@gmail.com>
2023-06-17 10:48:01 +0200

    :poop: starting Dropdown component

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -1,13 +1,36 @@
 import React from 'react'
 import HeroBanner from '../Components/HeroBanner'
+import Dropdown from './Dropdown'
 
 export default function About() {
+  const dropdownText = [
+    {
+      title: 'Fiabilité',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis diam in nunc lobortis lacinia. Donec euismod, nisl vitae aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nunc eu nisl. Donec euismod, nisl vitae aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nunc eu nisl.'
+    },
+    {
+      title: 'Respect',
+      text: 'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.'
+    },
+    {
+      title: 'Service',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis diam in nunc lobortis lacinia. Donec euismod, nisl vitae aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nunc eu nisl. Donec euismod, nisl vitae aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nunc eu nisl.'
+    },
+    {
+      title: 'Sécurité',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis diam in nunc lobortis lacinia. Donec euismod, nisl vitae aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nunc eu nisl. Donec euismod, nisl vitae aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nunc eu nisl.'
+    }
+  ] 
+  
   return (
     <div>
        <HeroBanner 
             text=''
             about={true}
         />
+        {dropdownText.map((dropdown, index) => 
+            <Dropdown text={dropdown.text} title={dropdown.title} key={index} />
+        )}
     </div>
   )
 }
---
+++ src/About/Dropdown.jsx
@@ -0,0 +1,21 @@
+import './Dropdown.css'
+import React, {useState} from 'react'
+
+function Dropdown (props) {
+    const [open, setOpen] = useState(false)
+
+    return (
+        <div className='dropdown-container' onClick={() => setOpen((prev) => !prev)}>
+            <h3>
+                {props.title}
+                <img src="/src/assets/arrow.svg" alt="arrow" />
+            </h3>
+            {open && 
+            <div className='dropdown-content'>
+                <p>{props.text}</p>
+            </div>}
+        </div>
+  )
+}
+
+export default Dropdown
