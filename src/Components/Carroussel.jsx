bd031d65c2b3307f0e502ae16290944745ae23b7
davidbarbi3r <davidbarbi3r@gmail.com>
2023-06-19 16:20:30 +0200

    :sparkles: feat: Carroussel ok

--- src/Components/Carroussel.jsx
+++ src/Components/Carroussel.jsx
@@ -1,43 +1,54 @@
-import React from 'react'
+import "./Carroussel.css";
+import React from "react";
 
 function Carroussel(props) {
-  const [currentImage, setCurrentImage] = React.useState(0)
+  const [currentImage, setCurrentImage] = React.useState(0);
 
-    function handleLeftArrowClick() {
-        setCurrentImage((currentImage) => {
-            if (currentImage === 0) {
-                return props.images.length - 1
-            } else {
-                return currentImage - 1
-            }
-        })
-    }
+  function handleLeftArrowClick() {
+    setCurrentImage((currentImage) => {
+      if (currentImage === 0) {
+        return props.images.length - 1;
+      } else {
+        return currentImage - 1;
+      }
+    });
+  }
 
-    function handleRightArrowClick() {
-        setCurrentImage((currentImage) => {
-            if (currentImage === props.images.length - 1) {
-                return 0
-            } else {
-                return currentImage + 1
-            }
-        })
-    }
+  function handleRightArrowClick() {
+    setCurrentImage((currentImage) => {
+      if (currentImage === props.images.length - 1) {
+        return 0;
+      } else {
+        return currentImage + 1;
+      }
+    });
+  }
 
   return (
-    <div className='carroussel-container'>
-        <div className='carroussel'>
-             <div className='carroussel-item'>
-                    <img src={props.images[currentImage]} alt='carroussel-item'/>
-                </div>
-            <div className='left-arrow' onClick={() => handleLeftArrowClick()}>
-                <img src='../assets/arrow.svg' alt='left-arrow'/>
-            </div>
-            <div className='right-arrow' onClick={() => handleRightArrowClick()}>
-                <img src='../assets/arrow.svg' alt='right-arrow'/>
-            </div>
+    <div className="carroussel-container">
+      <div className="carroussel">
+        <div className="carroussel-item">
+          <img src={props.images[currentImage]} alt="carroussel-item" />
         </div>
+        {props.images.length > 1 && (
+          <>
+            <button
+              className="left-arrow"
+              onClick={() => handleLeftArrowClick()}
+            >
+              <img src="/src/assets/arrow.svg" alt="left-arrow" />
+            </button>
+            <button
+              className="right-arrow"
+              onClick={() => handleRightArrowClick()}
+            >
+              <img src="/src/assets/arrow.svg" alt="right-arrow" />
+            </button>
+          </>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Carroussel
+export default Carroussel;
