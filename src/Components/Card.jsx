5d83b8477d3cac13a118a27092a57c54b193acb0
davidbarbi3r <davidbarbi3r@gmail.com>
2023-06-23 09:48:29 +0200

    :bug: make all card clickable

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -4,17 +4,15 @@ import { Link } from 'react-router-dom'
 
 function Card(props) {
   return (
-        <div className='card'>
-            <Link to={props.card.id}>
-                <div className="img-wrapper">
-                    <img src={props.card.cover} alt={props.card.title} />
-                </div>
-                <h3>
-                    {props.card.title}
-                </h3>
-            </Link>
+    <Link to={props.card.id}>
+      <article className="card">
+        <div className="img-wrapper">
+          <img src={props.card.cover} alt={props.card.title} />
         </div>
-  )
+        <h3>{props.card.title}</h3>
+      </article>
+    </Link>
+  );
 }
 
 export default Card
