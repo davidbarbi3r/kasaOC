4e45c59a1ffbff3be868c54c3038dc369e1df618
davidbarbi3r <davidbarbi3r@gmail.com>
2023-06-23 09:28:03 +0200

    :sparkles: use NavLink and active

--- src/Layout/Components/Navbar.jsx
+++ src/Layout/Components/Navbar.jsx
@@ -1,31 +1,26 @@
 import './Navbar.css'
 import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
-export default function Navbar () {
-    const { pathname } = useLocation();
-
-    return (
-      <div className="navbar-container">
-        <Link to="/">
-          <div className="navbar-logo">
-            <img src="/logo-K.svg" />
-            <img src="/logo-home.svg" />
-            <img src="/logo-S.svg" />
-            <img src="/logo-A.svg" />
-          </div>
-        </Link>
-        <ul>
-          <Link to="/" className={pathname === "/" ? "underline" : ""}>
-            <li>Accueil</li>
-          </Link>
-          <Link
-            to="a-propos"
-            className={pathname === "/a-propos" ? "underline" : ""}
-          >
-            <li>A propos</li>
-          </Link>
-        </ul>
-      </div>
-    );
+export default function Navbar() {
+  return (
+    <header className="navbar-container">
+      <Link to="/">
+        <div className="navbar-logo">
+          <img src="/logo-K.svg" />
+          <img src="/logo-home.svg" />
+          <img src="/logo-S.svg" />
+          <img src="/logo-A.svg" />
+        </div>
+      </Link>
+      <ul>
+        <li>
+          <NavLink to="/">Accueil</NavLink>
+        </li>
+        <li>
+          <NavLink to="a-propos">A propos</NavLink>
+        </li>
+      </ul>
+    </header>
+  );
 }
